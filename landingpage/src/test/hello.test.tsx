c571fbf3737b0299e3d848b8c776236e183e4629
martinmueller.dev <damadden88@googlemail.com>
2022-03-04 07:49:20 +0100

    fix: tests..

---
+++ landingpage/src/test/hello.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+test('soon', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  React.version;
+  // const app = new App();
+  // const stack = new MyStack(app, 'test');
+
+  // expect(stack).not.toHaveResource('AWS::S3::Bucket');
+  // expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
+});
